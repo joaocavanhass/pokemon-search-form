712cd62a9d1bd46ac6c3dfbbc990d946e4e14d91
joaocavanhass <joaopedro.cavanhascolombo@gmail.com>
2024-12-04 21:30:14 -0300

    finish style form

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -17,18 +17,18 @@ async function getPokemon(event) {
 
         const pokemon = await response.json()
 
-        infoContainer.innerHTML`
+        infoContainer.innerHTML = `
             <div class="card">
                 <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"> 
                 <h2>${pokemon.name}</h2>
                 <p><strong>Height:</strong>${pokemon.height / 10} m</p>
                 <p><strong>Weight:</strong>${pokemon.weight / 10} kg</p>
-                <p><strong>Types:</strong>${pokemon.types.map(type => type.type.name).join(', ')}}</p>
+                <p><strong>Types:</strong>${pokemon.types.map(type => type.type.name).join(', ')}</p>
             </div>
         `
     } catch(error) {
         console.error('Error', error)
-        infoContainer.innerHTML`
+        infoContainer.innerHTML = `
             <p>Erro: Pokemon not found, try again in a few minutes...</p>
         `
     }
